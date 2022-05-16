07f6b7e0b797603d15cc52c9e0989615adb07d3a
Adphi <philippe.adrien.nousse@gmail.com>
2022-05-16 12:07:15 +0200

    renamed python package
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open("README.md", encoding = "utf-8") as f:
 
 # Package description
 setup(
-    name = "mkdocs-material",
+    name = "mkdocs-linka-cloud",
     version = package["version"],
     url = "https://linka.cloud",
     license = package["license"],
